e899d8837fccecdf004a7050d06bd184299809bd
Scarlett-Truong <scarlett.truong307@gmail.com>
2018-09-26 16:51:06 -0700

    Complete API call for Lighthouse, GTMetrix, WebPageTest

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const WebPageTest = require('WebPageTest');
 //------------------GTMETRIX API-------------------------------
 // promise
 // Poll test every 5 seconds for completion, then log the result
-/*
+
 const gtmetrix = require ('gtmetrix') ({
   email: 'truongt8@student.douglascollege.ca',
   apikey: 'eb5db35e7e792206a5c192b4dc1af037'
@@ -40,8 +40,7 @@ const gtmResult = new Promise((resolve, reject) => {
         gtm.loadTime = response.results.fully_loaded_time/1000;
         gtm.pageSize = response.results.page_bytes/1000;
         gtm.requests = response.results.page_elements;
-        console.log(gtm.loadTime);
-        console.log(response);
+        console.log("GTMetrix has been called");
         resolve(gtm);
       }));
   } catch (e) {
@@ -49,6 +48,7 @@ const gtmResult = new Promise((resolve, reject) => {
   }
 });
 
+/*
 gtmResult.then( (gtmValues) => {
   console.log('print new gtm values');
   console.log(gtmValues);
@@ -56,6 +56,7 @@ gtmResult.then( (gtmValues) => {
   console.log(e);
 });
 */
+
 /*
 gtmResult.then((newGtm) => {
   console.log('print new gtm values');
@@ -64,7 +65,7 @@ gtmResult.then((newGtm) => {
 */
 
 //------------------WEBPAGETEST API-------------------------------
-/*
+
 let wpt = {
   startRender: 0,
   speedIndex: 0,
@@ -94,7 +95,7 @@ const wptResult = new Promise((resolve, reject) => {
   })
 });
 //wptResult.then(data => {console.log(data)})
-*/
+
 
 //------------------OAUTH2 AUTHENTICATION-------------------------------
 const jwToken = new google.auth.JWT(
@@ -115,9 +116,9 @@ jwToken.authorize((authErr) => {
 
 //Create CSV object and save as CSV file
 const fields = ["Date",
-                "Speed Index",
+                "Lighthouse Speed Index",
                 "Time To Interact",
-                "Fully Loaded Time",
+                "GT Fully Loaded Time",
                 "Total Page Size",
                 "Requests",
                 "Pages Image Score",
@@ -130,73 +131,64 @@ const fields = ["Date",
                 "Doc Completed kBytes",
                 "Fully Loaded Time",
                 "Fully Loaded kBytes" ];
-//wptResult.then(wptData => {
-  const myReport = [
-    {
-      "Date": new Date(),
-      "Speed Index": jsonData.audits["speed-index"].displayValue,
-      "Time To Interact": jsonData.audits.interactive.displayValue,
-      "Fully Loaded Time": "",
-      "Total Page Size": "",
-      "Requests": "",
-      "Pages Image Score": "",
-      "URL": "",
-      "Total Images Analyzed": "",
-      "Total Images Weight": "",
-      "Start Render": "wptData.startRender",
-      "Speed Index": "wptData.speedIndex",
-      "Doc Completed Time": "wptData.docTime",
-      "Doc Completed kBytes": "wptData.docKBytes",
-      "Fully Loaded Time": "wptData.fullTime",
-      "Fully Loaded kBytes": "wptData.fullKBytes",
-    }
-    // {
-    //   "name": jsonData.audits["speed-index"].title,
-    //   "type": "numeric",
-    //   "value": jsonData.audits["speed-index"].displayValue
-    // }, 
-    // {
-    //   "name": jsonData.audits.interactive.title,
-    //   "type": "numeric",
-    //   "value": jsonData.audits.interactive.displayValue
-    // }
-  ];
-   
-  const json2csvParser = new Json2csvParser({ fields });
-  const csv = json2csvParser.sparse(myReport);
-   
-  //console.log(csv);
-  
-  fs.writeFile('name.csv', csv, function (err) {
-      if (err) {
-          return console.log(err);
+        
+gtmResult.then(gtmData => {
+  wptResult.then(wptData => {
+    const myReport = [
+      {
+        "Date": new Date(),
+        "Lighthouse Speed Index": jsonData.audits["speed-index"].displayValue,
+        "Time To Interact": jsonData.audits.interactive.displayValue,
+        "GT Fully Loaded Time": gtmData.loadTime,
+        "Total Page Size": gtmData.pageSize,
+        "Requests": gtmData.requests,
+        "Pages Image Score": "",
+        "URL": "",
+        "Total Images Analyzed": "",
+        "Total Images Weight": "",
+        "Start Render": wptData.startRender,
+        "Speed Index": wptData.speedIndex,
+        "Doc Completed Time": wptData.docTime,
+        "Doc Completed kBytes": wptData.docKBytes,
+        "Fully Loaded Time": wptData.fullTime,
+        "Fully Loaded kBytes": wptData.fullKBytes,
       }
-      console.log('FILE SUCCESSFULLY WRITTEN!\n');
+    ];
+    
+    const json2csvParser = new Json2csvParser({ fields });
+    const csv = json2csvParser.parse(myReport);
+    //console.log(csv);
+    fs.writeFile('name.csv', csv, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('FILE SUCCESSFULLY WRITTEN!\n');
+    });
+    
+    
+    const folderId = "1-pkCn-ryOCPL8RAsi2j8SwPa9RYNNx6Q";
+    const fileMetadata = {
+        'name': 'My Report 2',
+        'mimeType': 'application/vnd.google-apps.spreadsheet',
+        parents: [folderId]
+    };
+    const media = {
+      mimeType: 'text/csv',
+      body: fs.createReadStream(path.join(__dirname, './name.csv'))
+    };
+    
+    drive.files.create({
+      auth: jwToken,
+      resource: fileMetadata,
+      media: media,
+      fields: 'id'
+    }, function(err, file) {
+      if (err) {
+        // Handle error
+        console.error(err);
+      } else {
+        console.log('File Id: ', file.id);
+      } 
+    });
   });
-  
-  
-  const folderId = "1-pkCn-ryOCPL8RAsi2j8SwPa9RYNNx6Q";
-  const fileMetadata = {
-      'name': 'My Report',
-      'mimeType': 'application/vnd.google-apps.spreadsheet',
-      parents: [folderId]
-  };
-  const media = {
-    mimeType: 'text/csv',
-    body: fs.createReadStream(path.join(__dirname, './name.csv'))
-  };
-  
-  drive.files.create({
-    auth: jwToken,
-    resource: fileMetadata,
-    media: media,
-    fields: 'id'
-  }, function(err, file) {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      console.log('File Id: ', file.id);
-    } 
-  });
-//});
+})
